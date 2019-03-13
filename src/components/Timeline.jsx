7435b296176a9f3c212ec0946c96368b6a7fb844
jdmartins <joaodiogomartins@gmail.com>
2019-03-13 02:14:13 +0200

    add Class impl

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,65 +1,71 @@
-import React, { useEffect, useState, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import TimelineItem from './TimelineItem';
 import MockTimelineService from '../service/timeline/mock/MockTimelineService';
 
-const Timeline = () => {
-  const [state, setState] = useState({
+class Timeline extends React.Component {
+  timelineSubscription = null;
+  state = {
     items: MockTimelineService.getTimelineData(),
     // We place the new tweets on a buffer
     buffer: []
-  });
-
-  useEffect(() => {
-    const timelineSubscription = MockTimelineService.timeline$().subscribe(data => {
-      setState({ buffer: [data, ...state.buffer], items: state.items });
-      document.title = `(${state.buffer ? state.buffer.length + 1 : ''}) React App`;
-    });
-
-    // Clean up subscription on did unmount
-    return () => {
-      timelineSubscription.unsubscribe();
-    };
-  }, [state.buffer, state.items]);
-
-  const addToItems = () => {
-    // still impure
-    // empty the buffer and place the new items on the array
-    setState({ items: [...state.buffer, ...state.items], buffer: [] });
-    // scroll to top
-
-    window.scrollTo(0, 0);
   };
 
-  const handleRetweet = item => {
+  componentDidMount() {
+    this.timelineSubscription = MockTimelineService.timeline$().subscribe(data => {
+      this.setState({ buffer: [data, ...this.state.buffer], items: this.state.items });
+      document.title = `(${this.state.buffer ? this.state.buffer.length + 1 : ''}) React App`;
+    });
+  }
+
+  componentWillUnmount() {
+    this.timelineSubscription.unsubscribe();
+  }
+
+  addToItems = () => {
+    // empty the buffer and place the new items on the array
+    this.setState({ items: [...this.state.buffer, ...this.state.items], buffer: [] });
+    // scroll to top
+
+    window.scrollTo({ top: 0 });
+  };
+
+  handleRetweet = item => {
     const incrementRetweetCount = n =>
       n.id === item.id ? { ...n, retweets: item.retweets + 1 } : n;
 
-    setState({
-      buffer: state.buffer,
-      items: [...state.items.map(incrementRetweetCount)]
+    this.setState({
+      buffer: this.state.buffer,
+      items: [...this.state.items.map(incrementRetweetCount)]
     });
   };
 
-  const handleLike = item => {
+  handleLike = item => {
     const toggleLike = n => (n.id === item.id ? { ...n, liked: !n.liked } : n);
 
-    setState({
-      buffer: state.buffer,
+    this.setState({
+      buffer: this.state.buffer,
       // Performance not so great we need to map all over the list O(N)
-      items: [...state.items.map(toggleLike)]
+      items: [...this.state.items.map(toggleLike)]
     });
   };
 
-  return (
-    <Fragment>
-      {state.buffer.length > 0 && (
-        <button onClick={addToItems}>{state.buffer.length} new Messages</button>
-      )}
-      {state.items.map((item, k) => (
-        <TimelineItem key={k} item={item} handleLike={handleLike} handleRetweet={handleRetweet} />
-      ))}
-    </Fragment>
-  );
-};
+  render() {
+    return (
+      <Fragment>
+        {this.state.buffer.length > 0 && (
+          <button onClick={this.addToItems}>{this.state.buffer.length} new Messages</button>
+        )}
+        {this.state.items.map((item, k) => (
+          <TimelineItem
+            key={k}
+            item={item}
+            handleLike={this.handleLike}
+            handleRetweet={this.handleRetweet}
+          />
+        ))}
+      </Fragment>
+    );
+  }
+}
 
 export default Timeline;
